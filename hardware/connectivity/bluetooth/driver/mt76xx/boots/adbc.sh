12c1661d4ec7cc6ff83988ef3acf8ddb9a98e5c7
Zinadin Zidan <zidan44@pixelexperience.org>
2022-03-03 00:16:55 +0600

    vendor: mtk: hardware: connectivity: Drop oss mtk BT hals
    * Broken now.
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ia5dcc4e3c28b93d70c1a7c44f46ec7c96eb58eba

--- hardware/connectivity/bluetooth/driver/mt76xx/boots/adbc.sh
+++
@@ -1,2 +0,0 @@
-wget "http://mirrors.neusoft.edu.cn/android/repository/platform-tools_r23.0.1-linux.zip"
-unzip platform-tools_r23.0.1-linux.zip
--- hardware/connectivity/connectivity_package.mk
+++ hardware/connectivity/connectivity_package.mk
@@ -2,16 +2,9 @@
 PRODUCT_PACKAGES += \
     libwapi \
     libwifitest \
-    libbt-vendor \
-    libbluetooth_mtk \
-    libbluetoothem_mtk \
     wpa_supplicant.conf \
-    libbluetooth_relayer \
     libkeystore-wifi-hidl \
-    libbluetooth_mtk_pure \
     wpa_supplicant_overlay.conf \
     p2p_supplicant_overlay.conf \
     libkeystore-engine-wifi-hidl \
-    android.hardware.bluetooth@1.0-impl-mediatek \
-    android.hardware.bluetooth@1.0-service-mediatek \
     android.hardware.wifi@1.0-service-lazy-mediatek
